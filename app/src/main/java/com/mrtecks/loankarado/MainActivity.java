8c5d62ef93c03afcff88d902059eb100d8025bd8
mukul <mukulraw199517@gmail.com>
2020-08-06 13:28:37 +0530

    LKD

---
+++ app/src/main/java/com/mrtecks/loankarado/MainActivity.java
@@ -0,0 +1,58 @@
+package com.mrtecks.loankarado;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    CardView personal , business , homesalaried , homeself , propertysalaried , propertyself;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        personal = findViewById(R.id.cardView2);
+        business = findViewById(R.id.cardView);
+        homesalaried = findViewById(R.id.cardView4);
+        homeself = findViewById(R.id.cardView3);
+        propertysalaried = findViewById(R.id.cardView6);
+        propertyself = findViewById(R.id.cardView5);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setTitle("Loan Kara Do");
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
+
+        personal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this , Personal.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/mrtecks/loankarado/Personal.java
@@ -0,0 +1,98 @@
+package com.mrtecks.loankarado;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class Personal extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ViewPager pager;
+    TabLayout tabs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal);
+
+        toolbar = findViewById(R.id.toolbar);
+        pager = findViewById(R.id.pager);
+        tabs = findViewById(R.id.tabLayout);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setTitle("Personal Loan");
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
+
+
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager() , FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        pager.setAdapter(adapter);
+        tabs.setupWithViewPager(pager);
+
+
+    }
+
+    class PagerAdapter extends FragmentStatePagerAdapter
+    {
+        String[] titles = {
+                "Personal",
+                "Professional"
+        };
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        public PagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return new personal();
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+
+    public static class personal extends Fragment
+    {
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.personal , container , false);
+
+            return view;
+        }
+    }
+
+}
