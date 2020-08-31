b95039cd38cad962412f5862b7008d002962e385
mukul <mukulraw199517@gmail.com>
2020-08-31 20:37:41 +0530

    LKD

--- app/src/main/java/com/mrtecks/loankarado/Home.java
+++ app/src/main/java/com/mrtecks/loankarado/Home.java
@@ -2,6 +2,7 @@ package com.mrtecks.loankarado;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.style.AlignmentSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.santalu.autoviewpager.AutoViewPager;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
@@ -30,7 +37,7 @@ public class Home extends Fragment {
 
     CardView personal, business, homesalaried, homeself, propertysalaried, propertyself;
 
-    ImageView banner;
+    AutoViewPager banner;
 
     static MainActivity mainActivity;
 
@@ -133,9 +140,9 @@ public class Home extends Fragment {
             @Override
             public void onResponse(Call<bannerBean> call, Response<bannerBean> response) {
 
-                ImageLoader loader = ImageLoader.getInstance();
-                DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory(true).cacheOnDisk(true).resetViewBeforeLoading(false).build();
-                loader.displayImage(response.body().getData().get(0), banner, options);
+                PagerAdapter adapter = new PagerAdapter(getChildFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, response.body().getData());
+                banner.setAdapter(adapter);
+
 
             }
 
@@ -147,4 +154,50 @@ public class Home extends Fragment {
 
         return view;
     }
+
+    class PagerAdapter extends FragmentStatePagerAdapter {
+        List<String> banners = new ArrayList<>();
+
+        public PagerAdapter(@NonNull FragmentManager fm, int behavior, List<String> banners) {
+            super(fm, behavior);
+            this.banners = banners;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            page page = new page();
+            Bundle b = new Bundle();
+            b.putString("url", banners.get(position));
+            page.setArguments(b);
+            return page;
+        }
+
+        @Override
+        public int getCount() {
+            return banners.size();
+        }
+    }
+
+    public static class page extends Fragment {
+        ImageView image;
+        String url;
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.page, container, false);
+
+            url = getArguments().getString("url");
+
+            image = view.findViewById(R.id.imageView4);
+
+            DisplayImageOptions options = new DisplayImageOptions.Builder().cacheOnDisk(true).cacheInMemory(true).resetViewBeforeLoading(false).build();
+            ImageLoader loader = ImageLoader.getInstance();
+            loader.displayImage(url, image, options);
+
+            return view;
+        }
+    }
+
 }
